90f9843e6ae2e7d73e4bc2756548382cabb6b4c2
hugo <hugo.marian.etu@univ-lille.fr>
2026-01-26 14:32:56 +0100

    Build: Ajout du fichier de classe Technicien

---
+++ Application/src/main/java/Projet/Application/Technicien.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package Projet.Application;
+
+import java.io.Serializable;
+
+/**
+ * 
+ */
+
+@Entity
+public class Technicien implements Serializable {
+
+	/**
+	 * 
+	 */
+	public Technicien() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
