755fff8532f5c0a576b0cc14070c0e569822b69c
hugo <hugo.marian.etu@univ-lille.fr>
2026-02-01 18:50:28 +0100

    Build: Ajout de l'entité Agenda

---
+++ Application/src/main/java/Projet/Application/Agenda.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package Projet.Application;
+
+import java.io.Serializable;
+import java.util.Calendar;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+/**
+ * 
+ */
+
+@Entity
+public class Agenda implements Serializable {
+/**
+ * 
+ */
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private Calendar dateDisponibilite;
+	
+	private static final long serialVersionUID = 1L;
+	
+	public Agenda() {
+		super();
+	}
+
+	public int getId() {
+	    return this.id;
+	}
+
+	public void setId(int id) {
+	    this.id = id;
+	}
+
+	public Calendar getDateDisponibilite() {
+	    return this.dateDisponibilite;
+	}
+
+	public void setDateDisponibilite(Calendar dateDisponibilite) {
+	    this.dateDisponibilite = dateDisponibilite;
+	}
+
+
+}
