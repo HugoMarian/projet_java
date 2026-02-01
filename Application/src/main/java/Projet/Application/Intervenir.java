5382c00ba7a81d1507d0605f284a2359bfe3ce89
hugo <hugo.marian.etu@univ-lille.fr>
2026-02-01 18:52:48 +0100

    Build: Ajout de l'entité Intervenir

---
+++ Application/src/main/java/Projet/Application/Intervenir.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package Projet.Application;
+
+import java.io.Serializable;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+/**
+ * 
+ */
+
+@Entity
+public class Intervenir implements Serializable{
+
+    @EmbeddedId
+    private IntervenirPK id;
+
+    @Column
+    private Calendar dateIntervention;
+
+    @Column
+    private String statut;
+
+    @Column
+    private String description;
+
+    private static final long serialVersionUID = 1L;
+
+    public Intervenir(){
+        super();
+    }
+
+    
+    public IntervenirPK getClePrimaire() {
+        return this.id;
+    }
+
+    public void setId(IntervenirPK id) {
+        this.id = id;
+    }
+
+    public Calendar getDateIntervention(){
+        return this.dateIntervention;
+    }
+
+    public void setDateIntervention(Calendar dateIntervention){
+        this.dateIntervention = dateIntervention;
+    }
+
+    public String getStatut(){
+        return this.statut;
+    }
+
+    public void setStatut(String statut){
+        this.statut = statut;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+
+}
